43e523ba1a20c9e08f6dba8bedd3c9727497ddac
Gustavo T. Rudiger <gustavo@trudax.tech>
2023-05-25 23:13:05 -0300

    feat: 🎸 check for other file formats

--- lua/coverage/languages/javascript.lua
+++ lua/coverage/languages/javascript.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local Path = require("plenary.path")
+local scan = require("plenary.scandir")
 local common = require("coverage.languages.common")
 local config = require("coverage.config")
 local util = require("coverage.util")
@@ -14,14 +15,29 @@ M.summary = common.summary
 --- Loads a coverage report.
 -- @param callback called with results of the coverage report
 M.load = function(callback)
-    local javascript_config = config.opts.lang.javascript
-    local p = Path:new(javascript_config.coverage_file)
-    if not p:exists() then
-        vim.notify("No coverage file exists.", vim.log.levels.INFO)
-        return
-    end
+	local javascript_config = config.opts.lang.javascript
+	local p = Path:new(javascript_config.coverage_file)
 
-    callback(util.lcov_to_table(p))
+	if not p:exists() then
+		local files = scan.scan_dir("coverage", { depth = 1, search_pattern = ".lcov$" })
+		local count = #files
+
+		if count == 0 then
+			vim.notify("No coverage file exists.", vim.log.levels.INFO)
+			return
+		end
+
+		if count > 1 then
+			vim.notify("Found multiple lcov files. Use only one!", vim.log.levels.INFO)
+			return
+		end
+
+		p = Path:new(files[1])
+	end
+
+	vim.notify("Loaded coverage from " .. p:normalize(), vim.log.levels.INFO)
+
+	callback(util.lcov_to_table(p))
 end
 
 return M
